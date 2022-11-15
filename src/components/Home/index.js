88f46662d6009eafa4924b407423369599aa664d
Nouar SalhEddine <nouarsalheddine@gmail.com>
2022-11-15 17:52:19 +0100

    match Card

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import logo from './img/logo.svg'
-import {Card , Button} from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
+import qatar from './img/flag/qatar.jpg'
+import equador from './img/flag/equador.jpg'
 function Home() {
   return (
     <div className='home container'>
@@ -9,12 +11,16 @@ function Home() {
       <h2 className='m-4' >Coupe du Monde de la FIFA, Qatar 2022™</h2>
       <div className="row m-4">
       <Card>
-      <Card.Header as="h5">Featured</Card.Header>
-      <Card.Body>
-        <Card.Title>Special title treatment</Card.Title>
-        <Card.Text>
-          With supporting text below as a natural lead-in to additional content.
-        </Card.Text>
+          <Card.Header >
+            Groupe A <br />
+            AL Bayt Stadium
+            <span className='card-date'>20 nov.2022</span>
+          </Card.Header>
+      <Card.Body className='row'>
+        <Card.Title className='col-3 text-center'> <img className='flag' src={qatar} alt="flag" /> Qatar</Card.Title>
+        <Card.Title className='col-6 text-center'> <h5>-   -</h5> </Card.Title>
+        <Card.Title className='col-3 text-md-center'> <img className='flag' src={equador} alt="flag" />Equateur </Card.Title>
+       
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
