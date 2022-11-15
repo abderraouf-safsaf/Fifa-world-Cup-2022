28300c58c5edb247446e1473a27be4bc896f85b1
Nouar SalhEddine <nouarsalheddine@gmail.com>
2022-11-15 17:01:32 +0100

    home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import logo from './img/logo.svg'
+import {Card , Button} from 'react-bootstrap';
 function Home() {
   return (
-    <div>
-      <h1>this is home</h1>
+    <div className='home container'>
+      
+      <img src={logo} alt="logo" />
+      <h2 className='m-4' >Coupe du Monde de la FIFA, Qatar 2022™</h2>
+      <div className="row m-4">
+      <Card>
+      <Card.Header as="h5">Featured</Card.Header>
+      <Card.Body>
+        <Card.Title>Special title treatment</Card.Title>
+        <Card.Text>
+          With supporting text below as a natural lead-in to additional content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+      </div>
+      
     </div>
   )
 }
