dfa332dde8a5796d47db1e3fb5921401e3502cd0
Nouar SalhEddine <nouarsalheddine@gmail.com>
2022-11-19 00:58:22 +0100

    cls

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{ useState} from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -9,43 +9,63 @@ import Footer from "./components/footer";
 import Player from "./components/players/player";
 
 function App() {
-  
   const data2 = require("./Database/Players.json");
 
   const players = data2.joueurs;
   const data = require("./Database/matches.json");
   const ReelMatche = data.matches;
- const [points, setPoints] = useState(0);
- 
- 
- console.log( );
-   
+  const [points, setPoints] = useState(0);
+
+  useEffect(() => {
+    for (let i = 0; i < players.length; i++) {
+      
+      for (let j = 0; j < ReelMatche.length; j++) {
+        if (
+          ReelMatche[j].score[0] === players[i].matches[j].score[0] &&
+          ReelMatche[j].score[1] === players[i].matches[j].score[1]
+        ) {
+          players[i].points += 3;
+
+          
+        } else if (
+          (ReelMatche[j].score[0] > ReelMatche[j].score[1] &&
+            players[i].matches[j].score[0] > players[i].matches[j].score[1]) ||
+          (ReelMatche[j].score[1] > ReelMatche[j].score[0] &&
+            players[i].matches[j].score[1] > players[i].matches[j].score[0])
+        ) {
+          
+          players[i].points += 1;
+        }
+      }
+     
+    }
+    
+  });
+
   function addClassment() {
     for (let i = 0; i < players.length; i++) {
       for (let j = 0; j < players[0].matches.length; j++) {
-
         if (ReelMatche[i].score !== null) {
-          if (ReelMatche[j].score[0] === players[i].matches[j].score[0] && ReelMatche[j].score[1] === players[i].matches[j].score[1]) {
-            
-            
-           setPoints(points +3)
-          return  
-          
-          
-        } else if (
-          (ReelMatche[j].score[0] > ReelMatche[j].score[1] && players[i].matches[j].score[0] > players[i].matches[j].score[1]) ||
-          (ReelMatche[j].score[1] > ReelMatche[j].score[0] && players[i].matches[j].score[1] > players[i].matches[j].score[0])
+          if (
+            ReelMatche[j].score[0] === players[i].matches[j].score[0] &&
+            ReelMatche[j].score[1] === players[i].matches[j].score[1]
           ) {
-            
-            setPoints(points +1)
-            return 
-          } 
+            setPoints(points + 3);
+            return;
+          } else if (
+            (ReelMatche[j].score[0] > ReelMatche[j].score[1] &&
+              players[i].matches[j].score[0] >
+                players[i].matches[j].score[1]) ||
+            (ReelMatche[j].score[1] > ReelMatche[j].score[0] &&
+              players[i].matches[j].score[1] > players[i].matches[j].score[0])
+          ) {
+            setPoints(points + 1);
+            return;
+          }
         }
       }
-      } 
-      }
-      
-      
+    }
+  }
 
   return (
     <BrowserRouter>
@@ -55,20 +75,21 @@ function App() {
         <Route path="/Fifa-world-Cup-2022/" element={<Home />} />
         <Route path="/" element={<Home />} />
 
-        <Route path="/landing" element={<Landing players={players} flag={players.flag} classment={points}
-         
-        />} />
+        <Route
+          path="/landing"
+          element={
+            <Landing players={players} flag={players.flag} classment={points} />
+          }
+        />
 
         {players.map((player, index) => {
-          
-          
           return (
             <Route
               key={index}
               path={`/${player.nom}`}
               element={
                 <Player
-                  index = {index}
+                  index={index}
                   players={players}
                   setClassment={setPoints}
                   classment={points}
